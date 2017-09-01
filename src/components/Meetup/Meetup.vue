3f6a9160829d3b1f3ff6132aeeb5f0d3b5b9709d
Boris <boris.chen@kdis.com>
2017-09-01 13:31:40 +1000

    create the basic route and basic hardcode template

---
+++ src/components/Meetup/Meetup.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-container>
+        <v-layout row wrap>
+            <v-flex xs12>
+                <v-card>
+                    <v-card-title>
+                       <h6 class="primary-text">My Meetups</h6>
+                    </v-card-title>
+                    <v-card-media
+                            src="https://thenypost.files.wordpress.com/2017/04/new-york.jpg?quality=90&strip=all&w=1200"
+                            height="400px"
+                    ></v-card-media>
+                    <v-card-text>
+                        <div class="info--text">01 09 2017 - New York</div>
+                        <div>
+                            asdasdasdasdasdsad
+                        </div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn class="primary">
+                            <!--<v-icon left light>arrow_forward</v-icon>-->
+                            Register</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Home from '@/components/Home'
+import Meetups from '@/components/Meetup/Meetups'
+import CreateMeetup from '@/components/Meetup/CreateMeetup'
+import Meetup from '@/components/Meetup/Meetup'
+import Profile from '@/components/User/Profile'
+import Signin from '@/components/User/Signin'
+import Signup from '@/components/User/Signup'
 
 Vue.use(Router)
 
@@ -8,8 +14,34 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/meetups',
+      component: Meetups
+    },
+    {
+      path: '/meetups/new',
+      component: CreateMeetup
+    },
+    {
+      path: '/meetups/:id',
+      name: 'Meetup',
+      component: Meetup
+    },
+    {
+      path: '/profile',
+      component: Profile
+    },
+    {
+      path: '/signin',
+      component: Signin
+    },
+    {
+      path: '/signup',
+      component: Signup
     }
-  ]
+  ],
+  mode: 'history'
 })
